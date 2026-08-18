90c256555a411f0c57afa67d4ef8a7ef37bf61e4
TXY05 <tehxiyuan@gmail.com>
2026-08-18 11:22:25 +0800

    Update Map

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,8 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+      GMSServices.provideAPIKey("AIzaSyBhmYk4prfvgk4Z22FZoEjjJ5cGO3erEUY")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
--- lib/screen/map.dart
+++ lib/screen/map.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -8,11 +10,62 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  GoogleMapController? mapController;
+
+  static const LatLng _center = LatLng(3.187308, 101.703697);
+
+  bool _locationPermissionGranted = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _requestLocationPermission();
+  }
+
+  Future<void> _requestLocationPermission() async {
+    // Check if GPS is enabled
+    final serviceEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!serviceEnabled) {
+      return;
+    }
+
+    // Check permission
+    LocationPermission permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    if (permission == LocationPermission.denied ||
+        permission == LocationPermission.deniedForever) {
+      return;
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _locationPermissionGranted = true;
+    });
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Map"),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Shelter Map'), elevation: 2),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: const CameraPosition(
+          target: _center,
+          zoom: 11.0,
+        ),
+        myLocationEnabled: _locationPermissionGranted,
+        myLocationButtonEnabled: true,
+      ),
     );
   }
 }
-
